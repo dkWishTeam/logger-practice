cfce499285b2f65515f516a5e31d3e5570999444
choijungwoo493 <jwoo1016@gmail.com>
2023-05-12 10:05:42 +0900

    initial commit

---
+++ src/test/java/com/example/loggerpractice/LoggerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loggerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
